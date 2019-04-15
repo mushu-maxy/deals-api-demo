3cc29e4ff8840beabdbfb18fd7a7872fbeac22da
MaxLoboda <max2loboda@gmail.com>
2019-04-15 11:52:16 +0300

    Add example to upload external signatures

--- README.md
+++ README.md
@@ -18,6 +18,11 @@ set deals_url=https://deals.dealssign.com
 set user=user name
 set pass=your password
 set org_code=00000000
+set signer=eMailSigner1@host.ua
+set signer2=eMailSigner2@host.ua
+set data_file=./testData/tst.PDF
+set signature=./testData/tst.PDF.p7s
+set signature2=./testData/tst.PDF.p7s
 
 npm run test
 ```
@@ -27,3 +32,10 @@ npm run test
 - **user** - Login of registered user
 - **pass** - Password
 - **org_code** - Code (EDRPO) of organization
+- **data_file** - Path to data file
+
+ Parameters for extended example
+- **signer** - First signer login
+- **signer2** - Second signer login
+- **signature** - Path to signature 1 file
+- **signature2** - Path to signature 2 file
--- test.js
+++ test.js
@@ -4,20 +4,28 @@
 console.log('Deals API test - start')
 const {DealsAPI} = require('@unitybase/deals-api')
 const fs = require('fs')
-const dealsUrl = process.env.deals_url || 'https://deals.dealssign.com'
-const user = process.env.user
-const pass = process.env.pass
-const orgCode = process.env.org_code
+const dealsUrl = process.env.deals_url || 'https://deals-demo.dealssign.com' ||  'https://deals-tst.deals.unitybase.info'
+const user = process.env.user || 'admin'
+const pass = process.env.pass || 'admin'
+const signer = process.env.signer
+const signer2 = process.env.signer2
+const signers = signer || signer2 ? [signer, signer2] : []
+const orgCode = process.env.org_code || '00000000'
+const dataFile = process.env.data_file
+const signature = process.env.signature
+const signature2 = process.env.signature2
 
 console.log('Deals URL:' + dealsUrl)
 console.log('User:' + user)
 console.log('Password:' + pass)
 console.log('Organization code:' + orgCode)
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+
 const api = new DealsAPI({dealsUrl: dealsUrl, login: user, password: pass})
 let dateFom = new Date()
 dateFom.setFullYear(dateFom.getFullYear() - 1)
-let docID
+let docID, dealID
 
 console.log('Get organization ID')
 api.getOrganization({code: orgCode}).then(res => {
@@ -29,25 +37,57 @@ api.getOrganization({code: orgCode}).then(res => {
     console.log('Deals list:')
     console.log(JSON.stringify(res, null, ' '))
     console.log('Add new deal')
-    return api.addNewDeal({name: 'deal for test1', description: (new Date()).toLocaleString()})
+    return api.addNewDeal({name: 'deal for test' + (new Date()).getTime(), description: (new Date()).toLocaleString()})
   })
   .then(res => {
     console.log('Deal ID: ' + res.ID)
+    dealID = res.ID
     console.log('Add new document')
-    return api.addDocument({dealID: res.ID, document: {name: 'test.js', docType: 'ONE_SIDE_SIGNING', requireConfirm: false, notifyWhenComplete: true}})
+    return api.addDocument({
+      dealID: res.ID,
+      document: {
+        name: 'test.js',
+        docType: 'ONE_SIDE_SIGNING',
+        requireConfirm: false,
+        notifyWhenComplete: true,
+        signers: signers
+      }
+    })
   })
   .then(res => {
+    console.log(res)
     docID = res[0]
     console.log('Document ID: ' + docID)
     console.log('Upload the document content')
-    let bufferData = fs.readFileSync('./test.js')
+    let bufferData = fs.readFileSync(dataFile)
     bufferData = bufferData.buffer.slice(bufferData.byteOffset, bufferData.byteOffset + bufferData.byteLength)
     return api.setDocumentContent({ ID: res[0], content: bufferData })
+  })
+  .then(res => {
+    if (!signature) return
+    console.log('Upload the 1 signature content')
+    let bufferData = fs.readFileSync(signature)
+    bufferData = bufferData.buffer.slice(bufferData.byteOffset, bufferData.byteOffset + bufferData.byteLength)
+    console.log({ ID: docID, signature: bufferData, user: signer })
+    return api.addDocumentSignature({ ID: docID, user: signer, signature: bufferData })
+  })
+  .then(res => {
+    if (!signature2) return
+    console.log('Upload the 2 signature content')
+    let bufferData = fs.readFileSync(signature2)
+    bufferData = bufferData.buffer.slice(bufferData.byteOffset, bufferData.byteOffset + bufferData.byteLength)
+    console.log({ ID: docID, signature: bufferData, user: signer2 })
+    return api.addDocumentSignature({ ID: docID, user: signer2, signature: bufferData })
   })
   .then(res => {
     console.log('done')
     console.log('Get the document information')
     return api.getDocumentInfo({ ID: docID })
+  })
+  .then(res => {
+    console.log(JSON.stringify(res, null, ' '))
+    console.log('Get the documents list')
+    return api.getDealDocumentList({dealID: dealID})
   })
   .then(res => {
     console.log(JSON.stringify(res, null, ' '))
